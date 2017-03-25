311fe27566055f9feb3310b238fba44686ba0e3e
Ian Fisher <ianfisher45@gmail.com>
2017-03-25 13:35:13 -0400

    Add DriverEvent and PassengerEvent models

--- ridematch/models.py
+++ ridematch/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class DriverEvent(models.Model):
+    name = models.CharField(max_length=20)
+    destination = models.CharField(max_length=50)
+    time = models.TimeField()
+    day = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __unicode__(self):
+        return '%s driving to %s at %s %s'.format(self.name, self.destination, self.day, self.time)
+
+class PassengerEvent(models.Model):
+    name = models.CharField(max_length=20)
+    destination = models.CharField(max_length=50)
+    time = models.TimeField()
+    day = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __unicode__(self):
+        return '%s driving to %s at %s %s'.format(self.name, self.destination, self.day, self.time)
