c12b3719aaf02589857fec8f148b6b702f4a65f5
Ian Fisher <ianfisher45@gmail.com>
2017-03-25 14:58:32 -0400

    Change date widget on form

--- ridematch/forms.py
+++ ridematch/forms.py
@@ -1,13 +1,14 @@
 import datetime
 from django.core.exceptions import ValidationError
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
 
 class RideshareForm(forms.Form):
     name = forms.CharField(label='', initial='Name', max_length=20)
     d_or_p = forms.ChoiceField(label='', choices=[('D', 'Driver'), ('P', 'Passenger')])
     destination = forms.CharField(label='', initial='Where to?', max_length=50)
     time = forms.TimeField(label='', initial=datetime.datetime.now().time)
-    day = forms.DateField(label='', initial=datetime.date.today)
+    day = forms.DateField(label='', initial=datetime.date.today, widget=forms.SelectDateWidget)
     email = forms.EmailField(label='', initial='Email')
     phone = forms.CharField(label='', initial='Phone number', max_length=20, required=False)
 
