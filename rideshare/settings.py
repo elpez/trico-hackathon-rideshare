d67d73bd20eca15f4c72c697353a029894cff9ad
Ian Fisher <ianfisher45@gmail.com>
2017-03-25 11:23:23 -0400

    Make ridematch app and basic template

--- rideshare/settings.py
+++ rideshare/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ridematch.apps.RidematchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
