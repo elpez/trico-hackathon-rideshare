916d5c1764b59d180b2ead2b0ce529142deca1d7
Ian Fisher <ianfisher45@gmail.com>
2017-03-25 19:35:48 -0400

    Fix bug with form

--- ridematch/forms.py
+++ ridematch/forms.py
@@ -8,34 +8,16 @@ hour_choices = zip(one_to_twelve, one_to_twelve)
 zero_to_fifty_nine = ['00', '15', '30', '45']
 min_choices = zip(zero_to_fifty_nine, zero_to_fifty_nine)
 
-current_hour = lambda: datetime.datetime.today().hour
-
-def next_hour():
-    hour = current_hour() + 1
-    return str(hour - 12) if hour > 12 else str(hour)
-
-def next_meridian():
-    """Return the meridian (AM or PM) it will be at the start of the next hour. Used as initial 
-       value of field on form.
-    """
-    return 'PM' if current_hour() + 1 > 12 else 'AM'
-
-def next_day():
-    """Return the day it will be at the start of the next hour. Used as initial value of field on 
-       form.
-    """
-    day = datetime.date.today()
-    if current_hour() == 23:
-        day = datetime.date(day.year, day.month, day.day + 1)
-    return day
+curr_hour = lambda: datetime.datetime.today().hour
+curr_meridian = lambda: 'PM' if curr_hour > 12 else 'AM'
 
 class RideshareForm(forms.Form):
     name = forms.CharField(label='', initial='Name', max_length=20)
     destination = forms.CharField(label='', initial='Where to?', max_length=50)
-    hour = forms.ChoiceField(label='', initial=next_hour, choices=hour_choices)
+    hour = forms.ChoiceField(label='', initial=curr_hour, choices=hour_choices)
     minute = forms.ChoiceField(label='', choices=min_choices)
-    meridian = forms.ChoiceField(label='', initial=next_meridian, 
+    meridian = forms.ChoiceField(label='', initial=curr_meridian, 
                                  choices=[('AM', 'AM'), ('PM', 'PM')])
-    day = forms.DateField(label='', initial=next_day, widget=forms.SelectDateWidget)
+    day = forms.DateField(label='', initial=datetime.datetime.today, widget=forms.SelectDateWidget)
     email = forms.EmailField(label='', initial='Email')
     phone = forms.CharField(label='', initial='Phone number', max_length=20, required=False)
