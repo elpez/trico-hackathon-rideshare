5a625cceed4263dc5056a619baa4e57c73a95489
Ian Fisher <ianfisher45@gmail.com>
2017-03-25 20:03:47 -0400

    more minor edits

--- ridematch/views.py
+++ ridematch/views.py
@@ -58,12 +58,12 @@ def finder_sharer_as_get(request, mode):
 
 def update_context(context, mode):
     if mode == 'finder':
-        context['table_title'] = 'Drivers available'
-        context['form_title'] = 'Make a post for a ride'
+        context['table_title'] = 'Drivers Available'
+        context['form_title'] = 'Make a Post for a Ride'
         context['obj_list'] = active_drivers()
     else:
-        context['table_title'] = 'Passengers looking for rides'
-        context['form_title'] = 'Offer your availability'
+        context['table_title'] = 'Passengers Looking for Rides'
+        context['form_title'] = 'Offer Your Availability'
         context['obj_list'] = active_passengers()
 
 def active_passengers():
@@ -72,8 +72,12 @@ def active_passengers():
 def active_drivers():
     return DriverEvent.objects.exclude(date__lt=datetime.datetime.today())
 
+def same_day(date1, date2):
+    return date1.year == date2.year and date1.month == date2.month and date1.day == date2.day
+
 def check_for_match(passenger, driver):
-    if passenger.destination == driver.destination:
+    if passenger.destination.lower() == driver.destination.lower() and \
+       same_day(passenger.date, driver.date):
         date_str = driver.date.strftime('%A %m/%d/%y at %I:%M %p')
         send_mail(email_subject, email_body.format(driver, date_str), 'ianfisher45@gmail.com', 
                   [passenger.email])
